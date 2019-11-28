e2ecbb0ffaa2322475b968ff6a321d574ef35249
Victor Nordam Suadicani <sortekanin@gmail.com>
2019-11-28 23:46:48 +0100

    Training!

--- src/datahandling.py
+++ src/datahandling.py
@@ -1,30 +1,27 @@
 from Bio import SeqIO
-from torch.utils.data import IterableDataset
+from torch.utils.data import IterableDataset, DataLoader
 import torch
 
 def seq2idx(seq):
-  return [ord(c) - 65 for c in seq]
+    print("called")
+    return torch.Tensor([ord(c) - 65 for c in seq]).unsqueeze(-1)
 
 def idxs2seq(idxs):
-  return "".join([chr(i + 65) for i in idxs])
+    return "".join([chr(i + 65) for i in idxs])
 
-def fasta_loader(filepath):
-  raw = SeqIO.parse(filepath, "fasta")
-  data = map(lambda x: str(x.seq), raw)
-  
-  return data
-  
-def create_dataset(filepath):
-  data = fasta_loader(filepath)
-  encoded = map(seq2idx, data)
-  
-  return encoded
-  
 class ProteinDataset(IterableDataset):
-  def __init__(self, file):
-    super().__init__()
-    self.file = file
-    self.data = create_dataset(file)
-  
-  def __iter__(self):
-    return iter(self.data)
+    def __init__(self, file):
+        super().__init__()
+        self.file = file
+
+    def __iter__(self):
+        rawSeqs = SeqIO.parse(self.file, "fasta")
+        strSeqs = map(lambda x: str(x.seq), rawSeqs)
+        encodedSeqs = map(seq2idx, strSeqs)
+        return encodedSeqs
+
+def sequenceCollateFn(sequences):
+    return torch.nn.utils.rnn.pack_sequence(sequences, enforce_sorted = False)
+
+def getProteinDataLoader(proteinDataset, batch_size = 32):
+    return DataLoader(proteinDataset, batch_size = batch_size, collate_fn = sequenceCollateFn)
--- src/dummy.py
+++ src/dummy.py
@@ -1,17 +1,48 @@
+from pathlib import Path
+
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.nn.utils.rnn import pack_sequence
 
+from datahandling import ProteinDataset, getProteinDataLoader
+
 class Dummy(nn.Module):
-  
+
   def __init__(self):
     super().__init__()
     # self.lin = nn.Linear(407, 10)
     self.lstm = nn.LSTM(1, 5)
-  
+
   def forward(self, xb):
-    packed_xb = pack_sequence(xb)
-    out, (hidden, cell) = self.lstm(packed_xb)
-    
-    return F.relu(out)
+    out, (hidden, cell) = self.lstm(xb)
+    return F.relu(out.data[-1])
+
+# Define model
+dummy = Dummy()
+
+# Load data
+data_file = Path("../data/uniref50.fasta")
+protein_dataset = ProteinDataset(data_file)
+protein_dataloader = getProteinDataLoader(protein_dataset, batch_size = 16)
+
+# Define optimizer
+opt = torch.optim.Adam(dummy.parameters())
+
+# Define loss function
+loss_fn = F.mse_loss
+
+for i, xb in enumerate(protein_dataloader):
+    # Forward pass
+    pred = dummy(xb)
+
+    # Calculate loss
+    loss = loss_fn(pred, torch.ones(5))
+    print(loss.item())
+
+    # Calculate gradient which minimizes loss
+    loss.backward()
+
+    # Take optimizer step in the direction of the gradient and reset
+    opt.step()
+    opt.zero_grad()
