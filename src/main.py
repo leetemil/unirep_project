e5b6036c2c2f180d4c598a5cb9e77db7f8aa5b2b
Victor Nordam Suadicani <sortekanin@gmail.com>
2019-12-14 13:04:30 +0100

    UniRep to own file

--- src/main.py
+++ src/main.py
@@ -7,47 +7,10 @@ warnings.filterwarnings("ignore", category = UserWarning)
 
 import torch
 from torch import nn
-from torch.nn import functional as F
 
-from datahandling import ProteinDataset, getProteinDataLoader, idx2seq
+from unirep import UniRep
+from datahandling import ProteinDataset, getProteinDataLoader
 from constants import *
-
-class UniRef(nn.Module):
-    def __init__(self, embed_size, hidden_size, num_layers):
-        super().__init__()
-
-        # Define parameters
-        self.embed_size = embed_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-
-        # Define layers
-        self.embed = nn.Embedding(NUM_TOKENS, self.embed_size, padding_idx = PADDING_VALUE)
-        self.rnn = nn.LSTM(self.embed_size, self.hidden_size, num_layers = self.num_layers, batch_first = True)
-        self.lin = nn.Linear(self.hidden_size, NUM_INFERENCE_TOKENS)
-
-    def forward(self, xb, xb_lens):
-        # This is important to do for DataParallel. We need this length when padding the packed sequence again
-        longest_length = xb.size(1)
-
-        # Convert indices to embedded vectors
-        embedding = self.embed(xb)
-
-        # Convert padded batch to a packed sequence. This makes it so the LSTM does not compute anything for the pad values
-        packed_seqs = torch.nn.utils.rnn.pack_padded_sequence(embedding, xb_lens, batch_first = True, enforce_sorted = False)
-
-        # Output from RNN is also packed when given packed
-        packed_out, (hidden, cell) = self.rnn(packed_seqs)
-
-        # Convert packed sequence back into padded batch. Using the longest length from the original batch is important
-        # as when using DataParallel, the padded batch may be longer than the longest sequence in the batch,
-        # since the longest sequence may have been given to another GPU. Doing it this way ensures that all GPUs produce
-        # an output batch that is the same dimensions.
-        out, lens = torch.nn.utils.rnn.pad_packed_sequence(packed_out, total_length = longest_length, batch_first = True)
-
-        # Linear layer to convert from RNN hidden size -> inference tokens scores
-        linear = self.lin(out)
-        return linear
 
 # Get hardware information
 print("CUDNN version:", torch.backends.cudnn.version())
@@ -60,8 +23,7 @@ print(f"Found {NUM_GPUS} GPUs!")
 EMBED_SIZE = 10
 HIDDEN_SIZE = 64
 NUM_LAYERS = 4
-
-model = UniRef(EMBED_SIZE, HIDDEN_SIZE, NUM_LAYERS)
+model = UniRep(EMBED_SIZE, HIDDEN_SIZE, NUM_LAYERS)
 
 # Use DataParallel if more than 1 GPU!
 if MULTI_GPU:
@@ -81,8 +43,10 @@ for i in range(inner_model.num_layers):
     nn.utils.weight_norm(inner_model.rnn, f"bias_ih_l{i}")
     nn.utils.weight_norm(inner_model.rnn, f"bias_hh_l{i}")
 
+# Training parameters
 EPOCHS = 1000
 BATCH_SIZE = 1024
+NUM_BATCHES = 1 + (NUM_SEQUENCES // BATCH_SIZE)
 PRINT_EVERY = 1000
 SAVE_EVERY = 1000
 
@@ -101,6 +65,7 @@ opt = torch.optim.Adam(model.parameters())
 # Define loss function
 criterion = nn.CrossEntropyLoss(ignore_index = PADDING_VALUE)
 
+# Train
 for e in range(EPOCHS):
     epoch_start_time = time.time()
     for i, (xb, xb_lens) in enumerate(protein_dataloader):
@@ -119,14 +84,14 @@ for e in range(EPOCHS):
         # Calculate gradient which minimizes loss
         loss.backward()
 
-        # Take optimizer step in the direction of the gradient and reset gradient
-        opt.step()
-        opt.zero_grad()
-
         # Printing progress
-        avg_time = (time.time() - epoch_start_time) / (i + 1)
+        sequences_processed = (i + 1) * BATCH_SIZE
+        time_taken = time.time() - epoch_start_time
+        avg_time = (time_taken) / (i + 1)
+        batches_left = NUM_BATCHES - (i + 1)
+        eta = max(0, avg_time * batches_left)
         if (i % PRINT_EVERY) == 0:
-            print(f"Epoch: {e:3} Batch: {i:6} Loss: {loss.item():5.4f} avg. time: {avg_time:5.2f} progress: {100 * (i + 1) * BATCH_SIZE / NUM_SEQUENCES:6.2f}%")
+            print(f"Epoch: {e:3} Batch: {i:6} Loss: {loss.item():5.4f} avg. time: {avg_time:5.2f} ETA: {eta / 3600:5.2f} progress: {100 * sequences_processed / NUM_SEQUENCES:6.2f}%")
 
         # Saving
         if (i % SAVE_EVERY) == 0:
@@ -136,6 +101,12 @@ for e in range(EPOCHS):
                 "optimizer_state_dict": opt.state_dict(),
                 "loss": loss
             }, "model.torch")
+
+        # Take optimizer step in the direction of the gradient and reset gradient
+        # Should be done after saving, since we otherwise save a different model than the one we reported on
+        opt.step()
+        opt.zero_grad()
+
     epoch_end_time = time.time()
     epoch_time = epoch_end_time - epoch_start_time
     print(f"Epoch {e} with {i + 1} batches took {epoch_time / 3600:.2f} hours ({(epoch_time / (i + 1)) / 3600:.2f} per batch).")
