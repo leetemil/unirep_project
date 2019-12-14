dec1f04e0a1e8dc7b154830645794b072c94f1ae
Victor Nordam Suadicani <sortekanin@gmail.com>
2019-12-14 21:49:52 +0100

    Restructuring and MLSTM option

--- src/main.py
+++ src/main.py
@@ -9,39 +9,15 @@ import torch
 from torch import nn
 
 from unirep import UniRep
+from mlstmunirep import UniRep_mLSTM
 from datahandling import ProteinDataset, getProteinDataLoader
 from constants import *
 
-# Get hardware information
-print("CUDNN version:", torch.backends.cudnn.version())
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-NUM_GPUS = torch.cuda.device_count()
-MULTI_GPU = NUM_GPUS > 1
-print(f"Found {NUM_GPUS} GPUs!")
-
-# Define model
+# Options
+MLSTM = False
 EMBED_SIZE = 10
 HIDDEN_SIZE = 64
 NUM_LAYERS = 4
-model = UniRep(EMBED_SIZE, HIDDEN_SIZE, NUM_LAYERS)
-
-# Use DataParallel if more than 1 GPU!
-if MULTI_GPU:
-    model = nn.DataParallel(model)
-
-model.to(device)
-
-if MULTI_GPU:
-    inner_model = model.module
-else:
-    inner_model = model
-
-# Apply weight norm on LSTM
-for i in range(inner_model.num_layers):
-    nn.utils.weight_norm(inner_model.rnn, f"weight_ih_l{i}")
-    nn.utils.weight_norm(inner_model.rnn, f"weight_hh_l{i}")
-    nn.utils.weight_norm(inner_model.rnn, f"bias_ih_l{i}")
-    nn.utils.weight_norm(inner_model.rnn, f"bias_hh_l{i}")
 
 # Training parameters
 EPOCHS = 1000
@@ -49,6 +25,38 @@ BATCH_SIZE = 1024
 NUM_BATCHES = 1 + (NUM_SEQUENCES // BATCH_SIZE)
 PRINT_EVERY = 1000
 SAVE_EVERY = 1000
+
+# Get hardware information
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+NUM_GPUS = torch.cuda.device_count()
+MULTI_GPU = NUM_GPUS > 1
+print(f"Found {NUM_GPUS} GPUs!")
+print("CUDNN version:", torch.backends.cudnn.version())
+
+# Define model
+if MLSTM:
+    model = UniRep_mLSTM(EMBED_SIZE, HIDDEN_SIZE, NUM_LAYERS)
+else:
+    model = UniRep(EMBED_SIZE, HIDDEN_SIZE, NUM_LAYERS)
+
+# Use DataParallel if more than 1 GPU!
+if MULTI_GPU:
+    model = nn.DataParallel(model)
+
+model.to(device)
+
+# Apply weight norm on LSTM
+if not MLSTM:
+    if MULTI_GPU:
+        inner_model = model.module
+    else:
+        inner_model = model
+
+    for i in range(inner_model.num_layers):
+        nn.utils.weight_norm(inner_model.rnn, f"weight_ih_l{i}")
+        nn.utils.weight_norm(inner_model.rnn, f"weight_hh_l{i}")
+        nn.utils.weight_norm(inner_model.rnn, f"bias_ih_l{i}")
+        nn.utils.weight_norm(inner_model.rnn, f"bias_hh_l{i}")
 
 # Load data
 # data_file = Path("../data/dummy/uniref-id_UniRef50_A0A007ORid_UniRef50_A0A009DWD5ORid_UniRef50_A0A009D-.fasta")
@@ -70,7 +78,11 @@ for e in range(EPOCHS):
     epoch_start_time = time.time()
     for i, (xb, xb_lens) in enumerate(protein_dataloader):
         # Forward pass
-        pred = model(xb, xb_lens)
+        if MLSTM:
+            init_hidden = [(torch.zeros(HIDDEN_SIZE, device = data_device), torch.zeros(HIDDEN_SIZE, device = data_device)) for _ in range(NUM_LAYERS)]
+            pred = model(xb, xb_lens, init_hidden)
+        else:
+            pred = model(xb, xb_lens)
 
         # Calculate loss
         true = torch.zeros(xb.shape, dtype = torch.int64, device = device) + PADDING_VALUE
