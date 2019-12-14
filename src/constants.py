7ccd1d0a59f51cc2fd5bbcdc77646e3bba29a204
Victor Nordam Suadicani <sortekanin@gmail.com>
2019-12-14 12:22:38 +0100

    Comments etc

--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,36 @@
+# Number of sequences in UniRef
+NUM_SEQUENCES = 39421231
+
+# The value in the target that should not be counted by cross-entropy loss
 INFERENCE_IGNORE_INDEX = -1
 
+# Special tokens in the protein sequences
 PAD = "<PAD>"
 EOS = "<EOS>"
-AMINO_ACIDS = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y"]
-CONTROL_TOKENS = [EOS, PAD]
-ALL_TOKENS = AMINO_ACIDS + CONTROL_TOKENS
 
-EXCLUDED_AMINO_ACIDS = "BJXZ"
+# Note: <PAD> should be last token since it is not ever guessed
+CONTROL_TOKENS = [EOS, PAD]
+
+# Non-ambigious amino acid characters
+AMINO_ACIDS = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y"]
+
+# All tokens that can be found in sequences, that is the amino acids and control tokens
+# Note: The order (Amino acids then control tokens) is important because <PAD> should be the last one
+# This ensures that 0 - NUM_INFERENCE_TOKENS are valid inference indices
+ALL_TOKENS = AMINO_ACIDS + CONTROL_TOKENS
 NUM_TOKENS = len(ALL_TOKENS)
+
+# Number of tokens that the model should be able to guess. -1 because pad should never be guessed
 NUM_INFERENCE_TOKENS = NUM_TOKENS - 1
 
-SEQ2IDX = {a: i for i, a in enumerate(ALL_TOKENS)}
-IDX2SEQ = {i: a for (a, i) in SEQ2IDX.items()}
+# These characters are ambigious amino acids, whatever that means (I'm not a biologist)
+EXCLUDED_AMINO_ACIDS = ["B", "J", "X", "Z"]
 
+# Dictionary mapping a sequence of tokens to a sequence of indices
+SEQ2IDX = {a: i for i, a in enumerate(ALL_TOKENS)}
+
+# Index of the pad token
 PADDING_VALUE = SEQ2IDX[PAD]
+
+# Dictionary mapping a sequence of indices to a sequence of tokens
+IDX2SEQ = {i: a for (a, i) in SEQ2IDX.items()}
--- src/main.py
+++ src/main.py
@@ -15,24 +15,41 @@ from constants import *
 class UniRef(nn.Module):
     def __init__(self, embed_size, hidden_size, num_layers):
         super().__init__()
+
+        # Define parameters
         self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
+        # Define layers
         self.embed = nn.Embedding(NUM_TOKENS, self.embed_size, padding_idx = PADDING_VALUE)
         self.rnn = nn.LSTM(self.embed_size, self.hidden_size, num_layers = self.num_layers, batch_first = True)
         self.lin = nn.Linear(self.hidden_size, NUM_INFERENCE_TOKENS)
 
     def forward(self, xb, xb_lens):
+        # This is important to do for DataParallel. We need this length when padding the packed sequence again
         longest_length = xb.size(1)
+
+        # Convert indices to embedded vectors
         embedding = self.embed(xb)
+
+        # Convert padded batch to a packed sequence. This makes it so the LSTM does not compute anything for the pad values
         packed_seqs = torch.nn.utils.rnn.pack_padded_sequence(embedding, xb_lens, batch_first = True, enforce_sorted = False)
+
+        # Output from RNN is also packed when given packed
         packed_out, (hidden, cell) = self.rnn(packed_seqs)
+
+        # Convert packed sequence back into padded batch. Using the longest length from the original batch is important
+        # as when using DataParallel, the padded batch may be longer than the longest sequence in the batch,
+        # since the longest sequence may have been given to another GPU. Doing it this way ensures that all GPUs produce
+        # an output batch that is the same dimensions.
         out, lens = torch.nn.utils.rnn.pad_packed_sequence(packed_out, total_length = longest_length, batch_first = True)
+
+        # Linear layer to convert from RNN hidden size -> inference tokens scores
         linear = self.lin(out)
         return linear
 
-# Get hardware
+# Get hardware information
 print("CUDNN version:", torch.backends.cudnn.version())
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 NUM_GPUS = torch.cuda.device_count()
@@ -46,28 +63,34 @@ NUM_LAYERS = 4
 
 model = UniRef(EMBED_SIZE, HIDDEN_SIZE, NUM_LAYERS)
 
-# This currently does not play nicely with data parallel
-# Apply weight norm on LSTM
-# for i in range(model.num_layers):
-#     nn.utils.weight_norm(model.rnn, f"weight_ih_l{i}")
-#     nn.utils.weight_norm(model.rnn, f"weight_hh_l{i}")
-#     nn.utils.weight_norm(model.rnn, f"bias_ih_l{i}")
-#     nn.utils.weight_norm(model.rnn, f"bias_hh_l{i}")
-
 # Use DataParallel if more than 1 GPU!
 if MULTI_GPU:
     model = nn.DataParallel(model)
 
 model.to(device)
 
+if MULTI_GPU:
+    inner_model = model.module
+else:
+    inner_model = model
+
+# Apply weight norm on LSTM
+for i in range(inner_model.num_layers):
+    nn.utils.weight_norm(inner_model.rnn, f"weight_ih_l{i}")
+    nn.utils.weight_norm(inner_model.rnn, f"weight_hh_l{i}")
+    nn.utils.weight_norm(inner_model.rnn, f"bias_ih_l{i}")
+    nn.utils.weight_norm(inner_model.rnn, f"bias_hh_l{i}")
+
 EPOCHS = 1000
-BATCH_SIZE = 1024
-PRINT_EVERY = 1000
-SAVE_EVERY = 1000
+BATCH_SIZE = 4
+PRINT_EVERY = 1
+SAVE_EVERY = 1
 
 # Load data
 # data_file = Path("../data/dummy/uniref-id_UniRef50_A0A007ORid_UniRef50_A0A009DWD5ORid_UniRef50_A0A009D-.fasta")
 data_file = Path("../data/UniRef50/uniref50.fasta")
+
+# If using DataParallel, data may be given from CPU (it is automatically distributed to the GPUs)
 data_device = torch.device("cuda" if torch.cuda.is_available() and not MULTI_GPU else "cpu")
 protein_dataset = ProteinDataset(data_file, data_device)
 protein_dataloader = getProteinDataLoader(protein_dataset, batch_size = BATCH_SIZE)
@@ -76,12 +99,11 @@ protein_dataloader = getProteinDataLoader(protein_dataset, batch_size = BATCH_SI
 opt = torch.optim.Adam(model.parameters())
 
 # Define loss function
-loss_fn = nn.CrossEntropyLoss(ignore_index = PADDING_VALUE)
+criterion = nn.CrossEntropyLoss(ignore_index = PADDING_VALUE)
 
 for e in range(EPOCHS):
-    total_time = 0
+    epoch_start_time = time.time()
     for i, (xb, xb_lens) in enumerate(protein_dataloader):
-        start_time = time.time()
         # Forward pass
         pred = model(xb, xb_lens)
 
@@ -89,22 +111,24 @@ for e in range(EPOCHS):
         true = torch.zeros(xb.shape, dtype = torch.int64, device = device) + PADDING_VALUE
         true[:, :-1] = xb[:, 1:]
 
+        # Flatten the sequence dimension to compare each timestep in cross entropy loss
         pred = pred.flatten(0, 1)
         true = true.flatten()
-        loss = loss_fn(pred, true)
+        loss = criterion(pred, true)
 
         # Calculate gradient which minimizes loss
         loss.backward()
 
-        # Take optimizer step in the direction of the gradient and reset
+        # Take optimizer step in the direction of the gradient and reset gradient
         opt.step()
         opt.zero_grad()
-        end_time = time.time()
-        loop_time = end_time - start_time
-        total_time += loop_time
-        if (i % PRINT_EVERY) == 0:
-            print(f"Epoch: {e:6} Batch: {i:6} Loss: {loss.item():5.4f} time: {loop_time:5.2f}, avg. time: {total_time / (i + 1):5.2f} progress: {100 * i * BATCH_SIZE / 39421231:7.3f}%")
 
+        # Printing progress
+        avg_time = (time.time() - epoch_start_time) / (i + 1)
+        if (i % PRINT_EVERY) == 0:
+            print(f"Epoch: {e:3} Batch: {i:6} Loss: {loss.item():5.4f} avg. time: {avg_time:5.2f} progress: {100 * (i + 1) * BATCH_SIZE / NUM_SEQUENCES:6.2f}%")
+
+        # Saving
         if (i % SAVE_EVERY) == 0:
             torch.save({
                 "iteration": i,
@@ -112,4 +136,6 @@ for e in range(EPOCHS):
                 "optimizer_state_dict": opt.state_dict(),
                 "loss": loss
             }, "model.torch")
-    print(f"Epoch {e} took {total_time / 3600:.2f} hours.")
+    epoch_end_time = time.time()
+    epoch_time = epoch_end_time - epoch_start_time
+    print(f"Epoch {e} with {i + 1} batches took {epoch_time / 3600:.2f} hours ({(epoch_time / (i + 1)) / 3600:.2f} per batch).")
